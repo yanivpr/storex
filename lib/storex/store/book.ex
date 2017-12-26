782ef62490918925a55353c37053aceb7ee2fb83
Yaniv Preiss <yanivpr@gmail.com>
2017-12-26 11:28:34 +0100

    Add cart functionality

--- lib/storex/store/book.ex
+++ lib/storex/store/book.ex
@@ -18,14 +18,5 @@ defmodule Storex.Store.Book do
     book
     |> cast(attrs, [:title, :description, :price, :image_url])
     |> validate_required([:title, :description, :price, :image_url])
-    |> validate_max_price()
-  end
-
-  def validate_max_price(changeset) do
-    price = get_change(changeset, :price)
-
-    if Decimal.cmp(price, 99.99) == :gt do
-      add_error(changeset, :price, "Price is not valid")
-    end
   end
 end
