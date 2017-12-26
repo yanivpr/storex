68ec23b803e87969099869153704e52b5738ee20
Yaniv Preiss <yanivpr@gmail.com>
2017-12-26 22:55:16 +0100

    sign up and in

--- lib/storex_web/router.ex
+++ lib/storex_web/router.ex
@@ -10,6 +10,7 @@ defmodule StorexWeb.Router do
     plug StorexWeb.Plugs.Cart
     plug StorexWeb.Plugs.ItemsCount
     plug StorexWeb.Plugs.ItemsPrice
+    plug StorexWeb.Plugs.CurrentUser
   end
 
   pipeline :api do
@@ -23,6 +24,8 @@ defmodule StorexWeb.Router do
     get "/books/:id", BookController, :show
 
     resources "/carts", CartController, singleton: true, only: [:show, :create, :delete]
+    resources "/users", UserController, only: [:new, :create]
+    resources "/sessions", SessionController, only: [:new, :create], singleton: true
   end
 
   # Other scopes may use custom stacks.
